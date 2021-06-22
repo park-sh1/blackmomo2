324084521975bbb5f1a72b8329b0170518a14326
shaprk <sungh90@naver.com>
2021-06-22 17:36:38 +0900

    layout 설계

---
+++ src/main/java/com/example/blackmomo/domain/Interest.java
@@ -0,0 +1,13 @@
+package com.example.blackmomo.domain;
+
+import lombok.Data;
+
+/**
+ * <p>게시판 관심분야</p>
+ */
+@Data
+public class Interest {
+
+    private String boardNo; // 게시물번호
+    private String interestNo; // 관심분야번호
+}
